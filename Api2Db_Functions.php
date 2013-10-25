a3af0f23b0b50246759cb763cd2487935dd1c631
darkrain <spiritkhl@gmail.com>
2013-10-25 16:31:18 +0700

    add post_request function

--- Api2Db_Functions.php
+++ Api2Db_Functions.php
@@ -256,4 +256,20 @@ class Api2Db_Functions
 		return $ret;
 	}
 
+    final public function post_request($url, $data) {
+
+        $ch = curl_init($url);
+
+
+        $postString = http_build_query($data, '', '&');
+
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postString);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $response = curl_exec($ch);
+        curl_close($ch);
+
+        return $response;
+   }
 }
